0922e1106f3c0c2a54aac4748f1f08a6accabeb6
Infernaught <89timw@gmail.com>
2023-11-07 20:07:33 -0500

    Add voice support

---
+++ frontend/assets/js/voice.js
@@ -0,0 +1,32 @@
+const SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+  
+const stat = document.getElementById("status"),
+    output = document.getElementById("result");
+
+function startRecognition() {
+    if (SpeechRecognition !== undefined) {
+    let recognition = new SpeechRecognition();
+
+    recognition.onstart = () => {
+        stat.innerHTML = `Currently listening: `;
+        output.classList.add("hide");
+    };
+
+    recognition.onspeechend = () => { // CHENGYU LOOK HERE!!! CHANGE THIS BEHAVIOR!!!
+        stat.innerHTML = `Stopped listening.`;
+        recognition.stop();
+    };
+
+    recognition.onresult = (result) => {
+        output.classList.remove("hide");
+        output.innerHTML = `I'm ${Math.floor(
+        result.results[0][0].confidence * 100
+        )}% certain you just said: <b>${result.results[0][0].transcript}</b>`;
+    };
+
+    recognition.start();
+    } else {
+    stat.innerHTML = "sorry not supported 😭";
+    }
+};
