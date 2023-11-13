3381dc0719e3aa165466d51f5106dbd5ae4007f5
Chengyu Sun <sun.990502@gmail.com>
2023-11-13 16:43:33 -0500

    add refresh to OS call

--- LF1.py
+++ LF1.py
@@ -24,7 +24,7 @@ def add_to_os(mapping):
     #    res = client.indices.create(index=INDEX)
     #except:
     #    pass
-    response = client.index(index=INDEX, body=mapping)
+    response = client.index(index=INDEX, body=mapping, refesh=True)
         
     return response
 
