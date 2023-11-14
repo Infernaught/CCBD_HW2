4d32229b9d12b03be422b121e741e64e56a3d8b8
Infernaught <72055086+Infernaught@users.noreply.github.com>
2023-11-14 13:50:20 -0700

    Update LF2.py

--- LF2.py
+++ LF2.py
@@ -64,8 +64,8 @@ def dispatch(event):
 
     # Initiate conversation with Lex
     response = client.recognize_text(
-            botId='QRLRNAADFW', # MODIFY HERE
-            botAliasId='PIAARW6SBI', # MODIFY HERE
+            botId='I3TNCJ7UW1', # MODIFY HERE
+            botAliasId='IWRZLZDYKW', # MODIFY HERE
             localeId='en_US',
             sessionId='testuser',
             text=msg_from_user)
